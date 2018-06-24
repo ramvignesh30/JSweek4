a6ed13a402c50429ca72cd60e80da0a655f521cc
vignesh srinivasan <ramvignesh30@gmail.com>
2018-06-24 13:50:42 +0200

    changed codes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-let 
+const carname = document.querySelector('.carname')
+const caryear = document.querySelector('.caryear')
+const caradd = document. querySelector('.caradd')
+
+let carList = []
+if(Window.localstorage.data !== undefined){
+    carList=carList.concat(JSON.parse(window.localStorage))
+}
+
+caradd.addEventListener('click', ()=>{
+    carList.push({
+name: carname.value,
+year:caryear.value
+
+    })
+    carname.value = ''
+})
